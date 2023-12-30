f1c7a45a60566f9e0232d72ec79fdf3bf58880b1
Albert Yumnam <84399334+Albx68@users.noreply.github.com>
2023-12-31 00:29:19 +0530

    show closest point

--- src/app/components/LineChart.tsx
+++ src/app/components/LineChart.tsx
@@ -6,26 +6,16 @@ import { item, range } from "../utils/types/TChart"
 type LineChartProps = {
     canvasHeight: number,
     canvasWidth: number,
-    pathOptions?: SVGProps<SVGPathElement>
+    pathOptions?: SVGProps<SVGPathElement>,
+    data: item[],
+    xRange: range,
+    yRange: range
 }
 
-const LineChart = ({ canvasHeight, canvasWidth, pathOptions }: LineChartProps) => {
+const LineChart = ({ canvasHeight, canvasWidth, pathOptions, data, xRange, yRange }: LineChartProps) => {
     const [selectedValue, setSelectedValue] = useState<number>()
-    const endTime = new Date().getTime()
-    const startTime = endTime - 60 * 1000 * 60 * 12 //12 hour gap
-    const timeGap = 60 * 1000 * 12 //12 minutes
-    const data = generateDummyData(startTime, endTime, timeGap)
-    const valueMin = Math.min(...data.map(item => item.value))
-    const valueMax = Math.max(...data.map(item => item.value))
 
-    const yRange = {
-        min: valueMin,
-        max: valueMax
-    }
-    const xRange = {
-        min: startTime,
-        max: endTime
-    }
+
     const pathPoints = generatePathPoints({ canvasHeight, canvasWidth, data, yRange, xRange })
     const pathString = generatePathString(pathPoints)
 
@@ -39,16 +29,21 @@ const LineChart = ({ canvasHeight, canvasWidth, pathOptions }: LineChartProps) =
     }
 
     const handleMouseMove: MouseEventHandler<SVGSVGElement> = (e) => {
-        console.log("mouse move", e)
+        const svgElement = e.currentTarget; // SVG element
+        const rect = svgElement.getBoundingClientRect();
 
+        // Calculate the relative coordinates within the SVG
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+        const closestPoint = findClosestPoint({ canvasWidth, data, x, xRange })
+        setSelectedValue(closestPoint?.value)
     }
     return <div>
         <svg width={canvasWidth} height={canvasHeight} onTouchMove={handleTouchMove} onMouseEnter={handleMouseEnter} onMouseMove={handleMouseMove}>
             <path d={pathString} fill="none" stroke={"#11ff99"} strokeWidth={8} strokeLinecap="round" {...pathOptions}>
             </path>
-
         </svg>
-        <div>{selectedValue}</div>
+        <div className="text-white">{selectedValue}</div>
     </div>
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
 import Image from 'next/image'
 import LineChart from './components/LineChart'
+import { generateDummyData } from './utils/helpers/ChartHelpers'
 
 export default function Home() {
+  const endTime = new Date().getTime()
+  const startTime = endTime - 60 * 1000 * 60 * 12 //12 hour gap
+  const timeGap = 60 * 1000 * 12 //12 minutes
+  const data = generateDummyData(startTime, endTime, timeGap)
+  const valueMin = Math.min(...data.map(item => item.value))
+  const valueMax = Math.max(...data.map(item => item.value))
+
+  const yRange = {
+    min: valueMin,
+    max: valueMax
+  }
+  const xRange = {
+    min: startTime,
+    max: endTime
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <LineChart canvasHeight={300} canvasWidth={600} />
+      <LineChart canvasHeight={300} canvasWidth={600} data={data} xRange={xRange} yRange={yRange} />
     </main>
   )
 }
