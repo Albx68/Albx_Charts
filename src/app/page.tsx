b6392a76deaf1e0215a30973de14d3e0a6f9a060
Albert Yumnam <84399334+Albx68@users.noreply.github.com>
2023-12-31 01:41:49 +0530

    use window dimension

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image'
+"use client"
 import LineChart from './components/LineChart'
 import { generateDummyData } from './utils/helpers/ChartHelpers'
 import { DateTime } from 'luxon'
+import useWindowDimensions from './utils/hooks/useWindowDimensions'
 
 export default function Home() {
+  const dimensions = useWindowDimensions()
   const dateTime = DateTime.local();
   const endTime = dateTime.toMillis();
   const startTime = endTime - 60 * 1000 * 60 * 12 //12 hour gap
@@ -12,18 +14,18 @@ export default function Home() {
   const valueMin = Math.min(...data.map(item => item.value))
   const valueMax = Math.max(...data.map(item => item.value))
   const verticalPadding = 20
-  const horizontalPadding = 20
+  const horizontalPadding = 60 * 1000 * 15
   const yRange = {
     min: valueMin - verticalPadding,
     max: valueMax + verticalPadding
   }
   const xRange = {
-    min: startTime,
-    max: endTime
+    min: startTime - horizontalPadding,
+    max: endTime + horizontalPadding
   }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <LineChart canvasHeight={300} canvasWidth={600} data={data} xRange={xRange} yRange={yRange} />
+      <LineChart canvasHeight={300} canvasWidth={dimensions.width} data={data} xRange={xRange} yRange={yRange} />
     </main>
   )
 }
