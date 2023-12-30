dd371e27a4b655d2eb5d050db26fc3a0d456d579
Albert Yumnam <84399334+Albx68@users.noreply.github.com>
2023-12-30 23:23:46 +0530

    added props for path

--- src/app/components/LineChart.tsx
+++ src/app/components/LineChart.tsx
@@ -1,13 +1,15 @@
 "use client"
 
+import { SVGProps } from "react"
 import { generateDummyData, generatePathString, generatePathPoints } from "../utils/helpers/ChartHelpers"
 
 type LineChartProps = {
     canvasHeight: number,
-    canvasWidth: number
+    canvasWidth: number,
+    pathOptions: SVGProps<SVGPathElement>
 }
 
-const LineChart = ({ canvasHeight = 200, canvasWidth = 400 }: LineChartProps) => {
+const LineChart = ({ canvasHeight = 200, canvasWidth = 400, pathOptions }: LineChartProps) => {
     const endTime = new Date().getTime()
     const startTime = endTime - 60 * 1000 * 60 * 12 //12 hour gap
     const timeGap = 60 * 1000 * 12 //12 minutes
@@ -26,7 +28,7 @@ const LineChart = ({ canvasHeight = 200, canvasWidth = 400 }: LineChartProps) =>
     const pathString = generatePathString(pathPoints)
     return <div>
         <svg width={canvasWidth} height={canvasHeight}>
-            <path d={pathString} fill="none" stroke="#11ff99" strokeWidth={8}>
+            <path d={pathString} fill="none" stroke={"#11ff99"} strokeWidth={8} strokeLinecap="round" {...pathOptions}>
             </path>
         </svg>
     </div>
