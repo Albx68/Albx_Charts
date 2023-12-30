cc8779f4075f0445c5ffd06dc010e43fd160a234
Albert Yumnam <84399334+Albx68@users.noreply.github.com>
2023-12-31 00:21:30 +0530

    find closest point

--- src/app/components/LineChart.tsx
+++ src/app/components/LineChart.tsx
@@ -1,8 +1,8 @@
 "use client"
 
-import { MouseEventHandler, SVGProps, TouchEventHandler } from "react"
-import { generateDummyData, generatePathString, generatePathPoints } from "../utils/helpers/ChartHelpers"
-
+import { MouseEventHandler, SVGProps, TouchEventHandler, useState } from "react"
+import { generateDummyData, generatePathString, generatePathPoints, getXinRange } from "../utils/helpers/ChartHelpers"
+import { item, range } from "../utils/types/TChart"
 type LineChartProps = {
     canvasHeight: number,
     canvasWidth: number,
@@ -10,12 +10,14 @@ type LineChartProps = {
 }
 
 const LineChart = ({ canvasHeight, canvasWidth, pathOptions }: LineChartProps) => {
+    const [selectedValue, setSelectedValue] = useState<number>()
     const endTime = new Date().getTime()
     const startTime = endTime - 60 * 1000 * 60 * 12 //12 hour gap
     const timeGap = 60 * 1000 * 12 //12 minutes
     const data = generateDummyData(startTime, endTime, timeGap)
     const valueMin = Math.min(...data.map(item => item.value))
     const valueMax = Math.max(...data.map(item => item.value))
+
     const yRange = {
         min: valueMin,
         max: valueMax
@@ -44,10 +46,31 @@ const LineChart = ({ canvasHeight, canvasWidth, pathOptions }: LineChartProps) =
         <svg width={canvasWidth} height={canvasHeight} onTouchMove={handleTouchMove} onMouseEnter={handleMouseEnter} onMouseMove={handleMouseMove}>
             <path d={pathString} fill="none" stroke={"#11ff99"} strokeWidth={8} strokeLinecap="round" {...pathOptions}>
             </path>
+
         </svg>
+        <div>{selectedValue}</div>
     </div>
 }
 
 export default LineChart
 
 
+const findClosestPoint = ({ x, canvasWidth, data, xRange }: { x: number, canvasWidth: number, data: item[], xRange: range }) => {
+    const xInRange = Math.max(x, 0)
+
+    const index = Math.round(
+        (xInRange /
+            getXinRange({
+                canvasWidth,
+                value: data[data.length - 1]!.time,
+                xRange
+            }) *
+            (data.length - 1)
+        ))
+    const pointIndex = Math.min(Math.max(index, 0), data.length - 1)
+
+    if (data[pointIndex]) {
+        return data[pointIndex]
+    }
+
+}
